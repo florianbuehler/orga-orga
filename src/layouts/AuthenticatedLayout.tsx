231d3fc7824bd9d38a4508b75202d2f0e6db16b5
Florian Bühler <florian-buehler@outlook.com>
2022-11-26 23:56:48 +0100

    Adapt sidebare styling.

--- src/layouts/AuthenticatedLayout.tsx
+++ src/layouts/AuthenticatedLayout.tsx
@@ -5,15 +5,19 @@ import { ThemeToggle, Logout } from 'components';
 const AuthenticatedLayout: React.FC = () => {
   return (
     <div className="drawer drawer-mobile">
-      <aside className="drawer-side bg-base-200 relative">
+      <aside className="drawer-side flex flex-col bg-base-200 relative">
+        <h1>OrgaOrga</h1>
         <ThemeToggle className="absolute top-2 right-2" />
         <label htmlFor="my-drawer-2" className="drawer-overlay"></label>
-        <ul className="menu p-4 w-80 text-base-content">
+        <ul className="menu mt-20 p-4 w-72 text-base-content flex-grow">
           <li>
-            <Logout />
+            <p>Sidebar Item 1</p>
           </li>
           <li>
             <p>Sidebar Item 2</p>
+          </li>
+          <li className="mt-auto">
+            <Logout />
           </li>
         </ul>
       </aside>
