96d39bfff6823322e176aff8537805ab6e6acb2b
Florian Bühler <florian-buehler@outlook.com>
2022-11-26 19:41:06 +0100

    Add browser router.

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,15 @@
 import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 function App() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<h1 className="text-3xl font-bold underline">Hello world!</h1>} />
+        <Route path="/login" element={<h1 className="text-3xl font-bold underline">Login</h1>} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
