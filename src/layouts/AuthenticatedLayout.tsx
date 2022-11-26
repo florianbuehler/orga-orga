586422b24450b56941b2fae4e20c44282e5c37aa
Florian Bühler <florian-buehler@outlook.com>
2022-11-27 00:19:42 +0100

    Improve logo.

--- src/layouts/AuthenticatedLayout.tsx
+++ src/layouts/AuthenticatedLayout.tsx
@@ -6,10 +6,10 @@ const AuthenticatedLayout: React.FC = () => {
   return (
     <div className="drawer drawer-mobile">
       <aside className="drawer-side flex flex-col bg-base-200 relative">
-        <h1>OrgaOrga</h1>
+        <h1 className="text-3xl p-4 italic">OrgaOrga</h1>
         <ThemeToggle className="absolute top-2 right-2" />
         <label htmlFor="my-drawer-2" className="drawer-overlay"></label>
-        <ul className="menu mt-20 p-4 w-72 text-base-content flex-grow">
+        <ul className="menu mt-12 p-4 w-72 text-base-content flex-grow">
           <li>
             <p>Sidebar Item 1</p>
           </li>
